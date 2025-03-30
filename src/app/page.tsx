32e02bcb75a3e814231ba1d108f8d675240fa6aa
Anayat khan <anayat0khan@gmail.com>
2025-03-30 22:40:06 +0530

    feat: feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturesSection from "@/components/features-9";
 import HeroSection from "@/components/hero-section";
 
 const LandingPage = () => {
   return (
     <div className="h-screen">
       <HeroSection />
+      <FeaturesSection />
     </div>
   );
 };
